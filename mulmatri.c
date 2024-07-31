bd8e13afb4b04febcd2934ec5317f5a292ae3797
ftf2004 <frickthefasak@outlook.com>
2024-07-31 09:50:04 +0000

    jhtg

--- mulmatri.c
+++ mulmatri.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
-
 int main() {
-    int r1, c1, r2, c2;
+    int r1, c1, r2, c2; int a[r1][c1], b[r2][c2], s[r1][c2];
     scanf("%d %d", &r1, &c1);
     scanf("%d %d", &r2, &c2);
-
-    int a[r1][c1], b[r2][c2], s[r1][c2];
-
     for (int i = 0; i < r1; i++) {
         for (int j = 0; j < c1; j++) {
             printf("%d x %d: ", i, j);
@@ -14,14 +10,12 @@ int main() {
         }
     }
     printf("\n");
-
     for (int i = 0; i < r2; i++) {
         for (int j = 0; j < c2; j++) {
             printf("%d x %d: ", i, j);
             scanf("%d", &b[i][j]);
         }
     }
-
     for (int i = 0; i < r1; i++) {
         for (int j = 0; j < c2; j++) {
             for (int k = 0; k < c1; k++) {
@@ -29,14 +23,10 @@ int main() {
             }
         }
     }
-
-    printf("Product of the matrices:\n");
     for (int i = 0; i < r1; i++) {
         for (int j = 0; j < c2; j++) {
             printf("%d ", s[i][j]);
         }
         printf("\n");
     }
-
-    return 0;
 }
