1aa4d08a555870d70b23c9478367e501c685fdd0
ftf2004 <frickthefasak@outlook.com>
2024-08-14 09:42:47 +0000

    fghfgb

--- recrev.c
+++ recrev.c
@@ -10,14 +10,9 @@ void rev(char str[], int start, int end) {
     rev(str, start + 1, end - 1);
 }
 
-int main() {
+void main() {
     char str[100];
-
     scanf("%s", str);
-
     rev(str, 0, strlen(str) - 1);
-
     printf("%s\n", str);
-
-    return 0;
 }
